6d59f80233126b7eb203f4463bfd30f6e0621191
Xiaozheng Xu <xiaozhengxu@Xiaozhengs-MacBook-Air.local>
2017-12-10 21:11:21 -0500

    Added Adder.py

--- adder.py
+++ adder.py
@@ -1,5 +1,102 @@
 from gates import *
 
-read1 = -1
-read2 = -1
-print (read1 == read2 and read1)
+class TernaryAdder:
+	def __init__(self):
+		self.triA = []
+		self.triB = []
+		self.operandA = [Wire() for i in range(9)]
+		self.operandB = [Wire() for i in range(9)]
+		self.decA = 0
+		self.decB = 0
+		self.overflows = [Wire() for i in range(9)]
+		self.result = [Wire() for i in range(9)]
+		self.resultRead = [ConnectionPoint(ConnectionPoint.READER) for i in range(9)]
+		self.gates = []
+		self.output = []
+		self.decRes = 0
+
+	def addTernary(self, Adigits, Bdigits):
+		"""Adigits: 
+			[0,0,-1,1,1,0,1,1,1]
+		   Bdigits:
+		   	[1,0,-1,1,1,0,0,0,1]
+		   The 0th element is the most significant trit
+		"""
+		if len(Adigits)<9:
+			for j in range(9-len(Adigits)):
+				Adigits.insert(0,0)
+		if len(Bdigits)<9:
+			padnum = 9-len(Bdigits)
+			for j in range(padnum):
+				Bdigits.insert(0,0)
+		assert len(Adigits) == 9 and len(Bdigits) == 9
+		self.triA = Adigits
+		self.triB = Bdigits
+		self.decA = self.convertToDecimal(Adigits)
+		self.decB = self.convertToDecimal(Bdigits)
+		#Connecting the wires to the input digits and result wires to the connection points:
+		for i in range(9):
+			aDigit= ConnectionPoint(ConnectionPoint.WRITER, state=Adigits[i])
+			self.operandA[i].Connect(aDigit)
+			bDigit= ConnectionPoint(ConnectionPoint.WRITER, state=Bdigits[i])
+			self.operandB[i].Connect(bDigit)
+			self.result[i].Connect(self.resultRead[i])
+
+		zero = ConnectionPoint(ConnectionPoint.WRITER, state = 0)
+		zeroWire = Wire()
+		zeroWire.Connect(zero)
+
+		#Doing the addition using 9 sum3 gates
+		for k in range(9):
+			curGate = GateSum3()
+			curGate.SetInputWire1(self.operandA[8-k])
+			curGate.SetInputWire2(self.operandB[8-k])
+			if k == 0:
+				curGate.SetInputWire3(zeroWire)
+			else:
+				curGate.SetInputWire3(self.overflows[k-1])
+			curGate.SetOutputWire(self.result[8-k])
+
+			print (curGate.ReadOutput())
+			
+			curGate.SetOverflowWire(self.overflows[k])
+			self.gates.append(curGate)
+		for j in range(9):
+			self.result[j].Update() #Update the wire, writing to the connection point
+		self.output = [cp.GetState() for cp in self.resultRead]
+		self.decRes = self.convertToDecimal(self.output)
+
+	def printResult(self):
+		print (self.triA)
+		print (self.triB)
+		print (self.output)
+		print (self.decA)
+		print (self.decB)
+		print (self.decRes)
+
+	def convertToDecimal(self, digits):
+		"""
+		digits is a list of -1, 0 and 1s, 
+		with the 0th element being the most significant (on the left).
+		"""
+		assert len(digits) == 9
+		result = 0
+		for i in range(9):
+			if digits[i] == 1:
+				result += 3**(8-i)
+			elif digits[i] == -1:
+				result -= 3**(8-i)
+		return result
+
+	def addDecimal(self, a, b):
+		addTernary(convertToTernary(a), convertToTernary(b))
+
+	def convertToTernary(self):
+		pass
+
+if __name__ == '__main__':
+	myadder = TernaryAdder()
+	myadder.addTernary([1,0,0,-1],[1,0,0,-1])
+	myadder.printResult()
+
+
--- gates.py
+++ gates.py
@@ -546,10 +546,85 @@ class GateSumAlternate(GateSum):
     self._output_gate.SetInputWire2(wires[10])
 
     # Consensus is the equivalent of an overflow/underflow
-    self._overflow_gate = GateConsensus()
+    self._overflow_gate = GateConsensusAlternate()
     self._overflow_gate.SetInputWire1(wires[0])
     self._overflow_gate.SetInputWire2(wires[1])
 
+  def SetInputWire1(self, wire):
+    self._identity_a.SetInputWire(wire)
+
+  def SetInputWire2(self, wire):
+    self._identity_b.SetInputWire(wire)
+
+  def SetOutputWire(self, wire):
+    self._output_gate.SetOutputWire(wire)
+
+  def SetOverflowWire(self, wire):
+    self._overflow_gate.SetOutputWire(wire)
+
+  def Update(self):
+    pass
+
+  def SetOverflowState(self):
+    pass
+
+
+class GateSum3(GateDiadic):
+  """
+  Sums three inputs (a, b, carryin), for use in adder
+  (+, +, +) => +,0 (3)
+  (-, -, -) => -,0 (-3)
+  (+, +, -) => 0,+ (1)
+  ...The carryout never creates its own carryover because the way the algebra is defined.
+  """
+  def __init__(self):
+    super().__init__()
+    self._input3 = ConnectionPoint(ConnectionPoint.READER, self)
+
+    wires = [Wire() for i in range(6)]
+    # a
+    self._identity_a = GateIdentity()
+    self._identity_a.SetOutputWire(wires[0])
+    # b
+    self._identity_b = GateIdentity()
+    self._identity_b.SetOutputWire(wires[1])
+    # carryin
+    self._identity_c = GateIdentity()
+    self._identity_c.SetOutputWire(wires[2])
+    # A = a+b () carry = (a+b)carry (wire 4)
+    sumAB = GateSumAlternate()
+    sumAB.SetInputWire1(wires[0])
+    sumAB.SetInputWire2(wires[1])
+    sumAB.SetOutputWire(wires[3])
+    sumAB.SetOverflowWire(wires[4])
+    # sum = (a+b)+c
+    self._sumAll = GateSumAlternate()
+    self._sumAll.SetInputWire1(wires[3])
+    self._sumAll.SetInputWire2(wires[2])
+    self._sumAll.SetOverflowWire(wires[5])
+    # carry = wire 4 + new carry (this can not create another carry)
+    self._overflow_gate = GateSumAlternate()
+    self._overflow_gate.SetInputWire1(wires[4])
+    self._overflow_gate.SetInputWire2(wires[5])
+
+  def SetInputWire3(self, wire):
+    self._identity_c.SetInputWire(wire)
+
+  def SetInputWire1(self, wire):
+    self._identity_a.SetInputWire(wire)
+
+  def SetInputWire2(self, wire):
+    self._identity_b.SetInputWire(wire)
+
+  def SetOutputWire(self, wire):
+    self._sumAll.SetOutputWire(wire)
+    wire.Connect(self._output)
+
+  def SetOverflowWire(self, wire):
+    self._overflow_gate.SetOutputWire(wire)
+
+  def Update(self):
+    pass
 
 class GateConsensusAlternate(GateDiadic):
   """
@@ -629,9 +704,6 @@ class GateConsensusAlternate(GateDiadic):
 
   def SetOutputWire(self, wire):
     self._output_gate.SetOutputWire(wire)
-
-  def SetOverflowWire(self, wire):
-    self._overflow_gate.SetOutputWire(wire)
 
   def Update(self):
     pass
